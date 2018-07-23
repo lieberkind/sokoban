7e8f94a9a72ca2256ccfbbcad1146c195a9e3cbe
Tomas Lieberkind <t.lieberkind@gmail.com>
2018-07-23 22:58:04 +0200

    Small refactoring

--- src/Views/Level.elm
+++ src/Views/Level.elm
@@ -3,22 +3,15 @@ module Views.Level exposing (renderLevel)
 import Array exposing (Array)
 import Html exposing (..)
 import Html.Attributes exposing (class)
-import Data.GameElement exposing (GameElement)
 import Views.GameElement
 import Data.Level as Level exposing (Level)
 
 
 renderLevel : Level -> Html msg
 renderLevel level =
-    printGrid (Level.grid level)
-
-
-
--- HELPERS
-
-
-printGrid : Array GameElement -> Html msg
-printGrid grid =
     div
         [ class "grid" ]
-        (Array.map Views.GameElement.renderGameElement grid |> Array.toList)
+        (Level.grid level
+            |> Array.toList
+            |> List.map Views.GameElement.renderGameElement
+        )
